059a85843f115f2c2f3a49b09445d8477341ab3d
zkkas <gabrielpcgamer23@gmail.com>
2025-11-22 19:21:05 -0300

    Resolvendo error upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }: { mode: string }) => {
+  return {
+    base: "/Wilson-Silva/",
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
     },
-  },
-}));
+    server: {
+      host: "::",
+      port: 8080,
+    },
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
